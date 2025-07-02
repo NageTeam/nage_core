efcac6c72a8bdaa86a4ac6db0df1ed851e588e61
Nage Team <nagecontract@gmail.com>
2025-07-03 00:22:14 +0200

    Update license.lua
    Fixed: Hackers could misuse the trigger to give license owner rank.

--- server/modules/license.lua
+++ server/modules/license.lua
@@ -11,16 +11,30 @@ local function getLicenseIdentifier(src)
 end
 
 AddEventHandler("playerConnecting", function(_, _, deferrals)
-    local src = source
-    local license = getLicenseIdentifier(src)
-
+    local nPlayer = source
+    local license = getLicenseIdentifier(nPlayer)
+    
     if license and not ownerAssigned then
         PerformHttpRequest("https://keymaster.fivem.net/api/licenses", function(status, body)
             if status == 200 and body then
                 if body:find(license:sub(9)) then
-                    TriggerEvent("nage:setPlayerRank", src, "owner")
-                    print(("[Nage Core] Auto-assigned OWNER rank to %s"):format(GetPlayerName(src)))
-                    ownerAssigned = true
+                    if not NAGE or not NAGE.TriggerCallback then
+                        print("^1[Nage Core]^7 TriggerCallback is not defined.")
+                        return
+                    end
+
+                    NAGE.TriggerCallback("nage:checkAdminAccess", nPlayer, function(isAdmin)
+                        if not isAdmin then
+                            TriggerClientEvent('nage_notify:notify', nPlayer, {
+                                title = locale["not_admin"],
+                                type = 'error'
+                            })
+                            return
+                        end
+
+                        TriggerEvent("nage:setPlayerRank", nPlayer, "owner")
+                        ownerAssigned = true
+                    end)
                 end
             end
         end, "GET", "", {
